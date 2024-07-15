6135cc493d41281578f4cd4767c4c5e7efba110c
unknown <padil2246@gmail.com>
2024-07-16 00:36:56 +0530

    first commit

---
+++ Day1.js
@@ -0,0 +1,74 @@
+// Day 1: Variable and Data Types
+
+
+// Tasks/Activities
+
+
+//Acivity 1: Variable Declaration
+
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console. 
+
+var a = 10;
+console.log(a);
+
+
+// Task 2: Declare a variable using let, assign it to a string, and log the value to the console. 
+
+let Name = "Adil";
+console.log(Name);
+
+
+//Acivity 2: Constant Declaration
+
+// Task 3: Declare a variable using const, assign it to a boolean value, and log the value to the console. 
+
+const bool = true;
+console.log(bool)
+
+//Acivity 3: Data Types
+
+// Task 4: Create a variable of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+
+// Number
+var number = 10;
+console.log(typeof(number));
+
+// String
+var fullname = "Adil Rasul Patel";
+console.log(typeof(fullname));
+
+// Boolean
+var boolean = false;
+console.log(typeof(boolean));
+
+// Object
+var obj = {
+    firstname: "Adil",
+    Age: 24,
+    isMarried: false
+};
+console.log(typeof(obj));
+
+// Array
+var arr = [10, 20, 30, 40, 50];
+console.log(typeof(arr));
+
+
+// Activity 4: Reassigning Variables
+
+// Task 5: Decalre a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+let b = 10;
+console.log(b);
+b = 20;
+console.log(b)
+
+
+// Activity 5: Understanding const
+
+// Task 5: Try reassigning a variable declared with const and observe the error.
+
+const c = 50;
+console.log(c);
+c = 60;
+console.log(c);
